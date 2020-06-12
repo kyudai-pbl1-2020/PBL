06098e8be4f94c4716645324757fe56d4cfde3c3
Nakamura Tsukasa <NakamuraTsukasa@poslnoMacBook-Pro-2.local>
2020-06-12 17:50:53 +0900

    add main()

--- sensor_test/sensor.py
+++ sensor_test/sensor.py
@@ -1,3 +1,6 @@
+import time
+import random
+
 class Sensor:
     """
     センサーの測定結果から商品の在庫が少なくなってきたことを検知する
@@ -19,9 +22,21 @@ class Sensor:
         :param measure: output from sensor
         """
         min_weight = self.weight * self.min_stock
-        if measure < min_weight:
+        if measure <= min_weight:
             return False
         else:
             return True
 
 
+def main():
+    item = "Water"
+    weight = 500 # g
+    min_stock = 3
+    water_sensor = Sensor( item, weight, min_stock )
+    for _ in range( 5 ): # while True:
+        measure = random.randrange( 1000, 3000, 1 ) # output from sensor
+        print( measure, water_sensor.stockcheck( measure ) )
+        time.sleep( 1 )
+
+if __name__ == "__main__":
+    main()
