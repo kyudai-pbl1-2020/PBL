1b475b72010c42f3a3201cc00865434425fb7325
ONourry <onourry3@gmail.com>
2020-06-26 14:10:05 +0900

    Removed unused function

--- PBL/View/MainUI.py
+++ PBL/View/MainUI.py
@@ -9,15 +9,3 @@ class MainUI(tk.Frame):
         self.grid()
         label = tk.Label(self, text="Start Page")
         label.grid(row = 0,column = 2)
-        self.GoogleSearch( "九大" )
-
-    def GoogleSearch( self, key_word ):
-        driver = webdriver.Chrome( executable_path = "./View/chromedriver" )
-        # get chromedriver from 
-        # https://sites.google.com/a/chromium.org/chromedriver/downloads
-        driver.get( "https://www.google.co.jp" )
-
-        elem = driver.find_element_by_name( "q" )
-        elem.send_keys( key_word )
-        elem.send_keys( Keys.ENTER )
-        driver.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div/div[1]/a/h3').click()
