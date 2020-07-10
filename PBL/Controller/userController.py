497e120d55eda61a1b0ef95a57cbc34b93673e85
Nakamura Tsukasa <NakamuraTsukasa@poslnoMacBook-Pro-2.local>
2020-07-10 18:15:32 +0900

    tested

--- PBL/Controller/userController.py
+++ PBL/Controller/userController.py
@@ -12,11 +12,10 @@ class UserController():
 
         # Set environment variables
         os.environ["AMAZON_ID"] = self.userid
-        os.environ["AMAZON_PW"] = self.pw
-
+        os.environ["AMAZON_PW"] = self.userpw
 
     def getUserInformation(self):
         # Get environment variables
         self.userid = os.getenv("AMAZON_ID")
-        self.pw = os.environ.get("AMAZON_PW")
-        return (self.userid, self.pw)
+        self.userpw = os.environ.get("AMAZON_PW")
+        return (self.userid, self.userpw)
