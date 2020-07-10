2713719346618ca89b29203535d7c46f03e6ffcc
ONourry <onourry3@gmail.com>
2020-07-10 13:49:24 +0900

    Refactored user.py as a controller

--- PBL/Controller/userController.py
+++ PBL/Controller/userController.py
@@ -1,21 +1,22 @@
 import os
 
-class User():
-
+class UserController():
     def __init__( self, id=None, pw=None):
         self.userid = id
         self.userpw = pw
+
 
     def setUserInformation(self, id, pw):
         self.userid = id
         self.userpw = pw
 
         # Set environment variables
-        os.environ[ "AMAZON_ID" ] = self.userid
-        os.environ[ "AMAZON_PW" ] = self.pw
-    
-    def getUserInformation( self ):
+        os.environ["AMAZON_ID"] = self.userid
+        os.environ["AMAZON_PW"] = self.pw
+
+
+    def getUserInformation(self):
         # Get environment variables
-        self.userid = os.getenv( "AMAZON_ID" )
-        self.pw = os.environ.get( "AMAZON_PW" )
+        self.userid = os.getenv("AMAZON_ID")
+        self.pw = os.environ.get("AMAZON_PW")
         return (self.userid, self.pw)
