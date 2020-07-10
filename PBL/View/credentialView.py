7b7b01af5f42f1e10ad3d17b40d902ec246aec7c
Nakamura Tsukasa <NakamuraTsukasa@poslnoMacBook-Pro-2.local>
2020-07-10 16:28:32 +0900

    print screen

--- PBL/View/credentialView.py
+++ PBL/View/credentialView.py
@@ -14,6 +14,7 @@ class UserController():
         os.environ["AMAZON_ID"] = self.userid
         os.environ["AMAZON_PW"] = self.userpw
 
+
     def getUserInformation(self):
         # Get environment variables
         self.userid = os.getenv("AMAZON_ID")
@@ -31,27 +32,28 @@ root.title( "Amazon ID/PW" )
 root.geometry( "400x300" )
 
 content = tk.Label( text = "Enter your Amazon ID/PW" )
-content.place( x = 0, y = 200 )
-content.pack()
+# content.pack()
+content.place( x = 125, y = 50 )
 
+id_label = tk.Label( text = "Amazon ID" )
+id_label.place( x = 50, y = 100 )
 id_box = tk.Entry()
-id_box.pack()
-id_value = id_box.get()
+id_box.place( x = 150, y = 100 )
 
+pw_label = tk.Label( text = "Amazon PW" )
+pw_label.place( x = 50, y = 150 )
 pw_box = tk.Entry()
-pw_box.pack()
-pw_value = pw_box.get()
+pw_box.place( x = 150, y = 150 )
 
-user_ctrl = UserController()
-user_ctrl.setUserInformation( id_value, pw_value )
+def getInfo( event ):
+    id_value = id_box.get()
+    pw_value = pw_box.get()
+    user_ctrl = UserController()
+    user_ctrl.setUserInformation( id_value, pw_value )
+    # print( user_ctrl.getUserInformation() )
 
-print( user_ctrl.getUserInformation() )
-
-class AddItemView( tk.Frame ):
-    def __init__( self, parent ):
-        tk.LabelFrame.__init__( self, parent )
-        self.parent = parent
-        self.grid()
+button = tk.Button( text = "DONE" )
+button.place( x = 175, y = 200 )
+button.bind( "<Button-1>", getInfo )
 
 root.mainloop()
-
