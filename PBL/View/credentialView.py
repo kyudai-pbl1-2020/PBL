1c11322664d63fbfa5f2ae5968bfe87c6b0a586d
Nakamura Tsukasa <NakamuraTsukasa@poslnoMacBook-Pro-2.local>
2020-07-10 17:09:39 +0900

    class

--- PBL/View/credentialView.py
+++ PBL/View/credentialView.py
@@ -1,59 +1,73 @@
-import os
+# import os
 
-class UserController():
-    def __init__( self, id=None, pw=None):
-        self.userid = id
-        self.userpw = pw
-
-
-    def setUserInformation( self, id, pw ):
-        self.userid = id
-        self.userpw = pw
-
-        # Set environment variables
-        os.environ["AMAZON_ID"] = self.userid
-        os.environ["AMAZON_PW"] = self.userpw
+# class UserController():
+#     def __init__( self, id=None, pw=None):
+#         self.userid = id
+#         self.userpw = pw
 
 
-    def getUserInformation(self):
-        # Get environment variables
-        self.userid = os.getenv("AMAZON_ID")
-        self.userpw = os.environ.get("AMAZON_PW")
-        return (self.userid, self.userpw)
+#     def setUserInformation( self, id, pw ):
+#         self.userid = id
+#         self.userpw = pw
+
+#         # Set environment variables
+#         os.environ["AMAZON_ID"] = self.userid
+#         os.environ["AMAZON_PW"] = self.userpw
+
+
+#     def getUserInformation(self):
+#         # Get environment variables
+#         self.userid = os.getenv("AMAZON_ID")
+#         self.userpw = os.environ.get("AMAZON_PW")
+#         return (self.userid, self.userpw)
 
 import tkinter as tk
-# from Controller.userController import UserController
+from PBL.View.credentialView import credentialView
+from PBL.Controller.userController import UserController
 import matplotlib
 matplotlib.use( "tkagg" )
 
-root = tk.Tk()
+class credentialView( tk.Frame ):
+    def __init__( self ):
+        pass
 
-root.title( "Amazon ID/PW" )
-root.geometry( "400x300" )
+    def getIDandPW( self ):
+        root = tk.Tk()
 
-content = tk.Label( text = "Enter your Amazon ID/PW" )
-# content.pack()
-content.place( x = 125, y = 50 )
+        root.title( "Amazon ID/PW" )
+        root.geometry( "400x300" )
 
-id_label = tk.Label( text = "Amazon ID" )
-id_label.place( x = 50, y = 100 )
-id_box = tk.Entry()
-id_box.place( x = 150, y = 100 )
+        content = tk.Label( text = "Enter your Amazon ID/PW" )
+        # content.pack()
+        content.place( x = 125, y = 50 )
 
-pw_label = tk.Label( text = "Amazon PW" )
-pw_label.place( x = 50, y = 150 )
-pw_box = tk.Entry()
-pw_box.place( x = 150, y = 150 )
+        id_label = tk.Label( text = "Amazon ID" )
+        id_label.place( x = 50, y = 100 )
+        id_box = tk.Entry()
+        id_box.place( x = 150, y = 100 )
 
-def getInfo( event ):
-    id_value = id_box.get()
-    pw_value = pw_box.get()
-    user_ctrl = UserController()
-    user_ctrl.setUserInformation( id_value, pw_value )
-    # print( user_ctrl.getUserInformation() )
+        pw_label = tk.Label( text = "Amazon PW" )
+        pw_label.place( x = 50, y = 150 )
+        pw_box = tk.Entry()
+        pw_box.place( x = 150, y = 150 )
 
-button = tk.Button( text = "DONE" )
-button.place( x = 175, y = 200 )
-button.bind( "<Button-1>", getInfo )
+        def getInfo( event ):
+            id_value = id_box.get()
+            pw_value = pw_box.get()
+            user_ctrl = UserController()
+            user_ctrl.setUserInformation( id_value, pw_value )
+            # print( user_ctrl.getUserInformation() )
+            return
 
-root.mainloop()
+        button = tk.Button( text = "DONE" )
+        button.place( x = 175, y = 200 )
+        button.bind( "<Button-1>", getInfo )
+
+        root.mainloop()
+
+def main():
+    creview = credentialView()
+    creview.getIDandPW()
+
+if __name__ == "__main__":
+    main()
