96bc6595e1dba6bbbfb9e5301229f016ecc9b740
郭月晴 <guoyueqing@Mooni.local>
2020-07-11 18:11:32 +0900

    just write a little

--- PBL/View/Components/itemComponent.py
+++ PBL/View/Components/itemComponent.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+#from PIL import Image
 
 class ItemComponent(tk.Frame):
 
@@ -6,28 +7,40 @@ class ItemComponent(tk.Frame):
         tk.LabelFrame.__init__(self, parent)
         self.parent = parent
         self.grid()
-
-        self.pic_frame = tk.Frame(self, background="light green")
+        #frame for checkbox, mage
+        self.pic_frame = tk.Frame(self)#, background="light green")
         self.pic_frame.grid(row=0, column=0, sticky="nsew")
-
-        self.info_frame = tk.Frame(self, background="pink")
+        #frame for name, weight, button
+        self.info_frame = tk.Frame(self)#, background="pink")
         self.info_frame.grid(row=0, column=1, sticky="nsew")
 
-        #self.item_state = tk.Stringvar()
-        self.active_contr = tk.Checkbutton(self.pic_frame)#, variable = self.item_state)
-        self.active_contr.grid(row=0,column=0, padx=15)
+        self.status = tk.StringVar()
+        self.status.set(1)
+        self.status_control = tk.Radiobutton(self.pic_frame, variable = self.status, value=0, command=self.activeItem)
+        self.status_control.grid(row=0, column=0, padx=15)
         #
+        #self.image_file = Image.open("")
+        #self.image_file.resize((100,100))
+        self.image_file = tk.PhotoImage(file="")
         self.pic_canvas = tk.Canvas(self.pic_frame, width=100, height=100, background="light gray")
         self.pic_canvas.grid(row=0, column=1, padx=15, pady=10)
+        self.pic_canvas.create_image(0,0, anchor="nw", image=self.image_file)
         #
-        self.name_label = tk.Label(self.info_frame, text="111", width=15, font=("Arial",12))
-        self.name_label.grid(row=0, padx=15, pady=8)
+        self.itemName_label = tk.StringVar()
+        self.name_label = tk.Label(self.info_frame, textvariable=self.itemName_label, width=15, font=("Arial",12))
+        self.name_label.grid(row=0, padx=15, pady=8, sticky="nsew")
         #
-        self.gauge_canvas = tk.Canvas(self.info_frame, width=150, height=20, background="light grey")
-        self.gauge_canvas.grid(row=1, padx=15, pady=8)
+        self.progressBar = tk.Canvas(self.info_frame, width=150, height=20, background="light grey")
+        self.progressBar.grid(row=1, padx=15, pady=8, sticky="nsew")
 
-        self.order_button = tk.Button(self.info_frame, text="Order", width=9, height=1, command=self.orderItem, font=("Arial",8))
+        self.current_weight = 100
+        self.progressBar.create_rectangle(0,0,self.current_weight,25, fill="#00C5CD")
+
+        self.order_button = tk.Button(self.info_frame, text="Order", width=12, height=2, command=self.orderItem, font=("Arial",12))
         self.order_button.grid(row=2, padx=15, pady=8)
 
-    def orderItem(self):
+    def orderItem(self): #order Button
+        pass
+
+    def activeItem(self):
         pass
