b36d40b5fe00fb10f93ebd2be6bebeaab3a0e68a
ONourry <onourry3@gmail.com>
2020-07-22 18:14:01 +0900

    making controller class

--- PBL/Controller/scaleController.py
+++ PBL/Controller/scaleController.py
@@ -1,56 +1,79 @@
-def getWeight(weight):
-	import subprocess
-	import time
-	import serial
-	# configure the serial connections (the parameters differs on the device you are connecting to)
-	ser = serial.Serial(
-	   port='/dev/ttyUSB0',
-	   baudrate=9600,
-	   timeout=1,
-	   parity=serial.PARITY_EVEN,
-	   stopbits=serial.STOPBITS_ONE,
-	   bytesize=serial.SEVENBITS
-	)
-	print(ser.isOpen())
-	ser.write(b'C\r')
-	time.sleep(0.1)
-	ser.write(b'C\r')
-	time.sleep(0.1)
-	ser.write(b'C\r')	
-	time.sleep(0.1)
-	ser.write(b'SIR\r')
-	time.sleep(0.1)
-	ser.write(b'SIR\r')
-	time.sleep(0.1)
-	ser.write(b'SIR\r')
+import subprocess
+import time
+import serial
 
-	def escaping(c):
-	    if c != '\r' and (ord(c)<0x20 or ord(c)> 0x7e):
-	        return ''
-	    return c
-	    
-	def dataprocess(esc):
-	    escaped_split = esc.split('\r')
-	    for line in escaped_split[:-1]:
-	        if line[0:2] != 'ST':
-	            continue
-	        try:
-	            weight = float(line[3:12])
-	        except:
-	            continue
-	        print('{:.2f}'.format(weight),flush=True)
-	    return escaped_split[-1]
-	            
-	scale = b''
-	escaped = ''
-	while True :
-	    if(ser.in_waiting != 0):
-	        data = ser.read(ser.in_waiting)
-	        scale += data
-	    if len(scale) > 0:
-	        escaped += ''.join(list(map(escaping,scale.decode())))
-	        escaped = dataprocess(escaped)
-	        #scale = b''
-	        break
-	ser.write(b'C\r')
-return weight
+
+class ScaleController:
+	def __init__(self):
+		self.port = '/dev/ttyUSB0'
+		self.baudrate = 9600
+		self.timeout = 1
+		self.parity = serial.PARITY_EVEN
+		self.stopbits = serial.STOPBITS_ONE
+		self.bytesize = serial.SEVENBITS
+		
+		self.ser = serial.Serial(
+			port=self.port,
+			baudrate=self.baudrate,
+			timeout=self.timeout,
+			parity=self.parity,
+			stopbits=self.stopbits,
+			bytesize=self.bytesize
+		)
+
+	def setup(self):
+		print(self.ser.isOpen())
+		self.ser.write(b'C\r')
+		time.sleep(0.1)
+		self.ser.write(b'C\r')
+		time.sleep(0.1)
+		self.ser.write(b'C\r')	
+		time.sleep(0.1)
+		self.ser.write(b'SIR\r')
+		time.sleep(0.1)
+		self.ser.write(b'SIR\r')
+		time.sleep(0.1)
+		self.ser.write(b'SIR\r')
+
+	def escaping(self,c):
+		if c != '\r' and (ord(c)<0x20 or ord(c)> 0x7e):
+			return ''
+		return c
+
+	def dataprocess(self,esc):
+		escaped_split = esc.split('\r')
+		for line in escaped_split[:-1]:
+			if line[0:2] != 'ST':
+				continue
+			try:
+				weight = float(line[3:12])
+			except:
+				continue
+			print('{:.2f}'.format(weight),flush=True)
+
+		return escaped_split[-1]
+
+
+	def getWeight(self):
+		self.setup()
+
+		scale = b''
+		escaped = ''
+
+		if(self.ser.in_waiting != 0):
+			data = self.ser.read(self.ser.in_waiting)
+			scale += data
+		if len(scale) > 0:
+			escaped += ''.join(list(map(self.escaping,scale.decode())))
+			escaped = self.dataprocess(escaped)
+			#scale = b''
+
+		self.ser.write(b'C\r')
+		return scale
+
+
+#TESTING
+if __name__ == "__main__":
+	sc = ScaleController()
+	currentWeight = sc.getWeight()
+	print(currentWeight)
