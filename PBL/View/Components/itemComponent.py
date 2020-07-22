43c68d8982bc175fbfe78f7f1472ab007ec80842
郭月晴 <guoyueqing@Mooni.local>
2020-07-22 17:16:22 +0900

    arrange components on mainUI

--- PBL/View/Components/itemComponent.py
+++ PBL/View/Components/itemComponent.py
@@ -3,25 +3,25 @@ from PIL import ImageTk, Image
 
 class ItemComponent(tk.Frame):
 
-    def __init__(self, parent,item):
+    def __init__(self, parent, item):
         tk.LabelFrame.__init__(self, parent)
         self.parent = parent
         self.grid()
-        #frame for checkbox, mage
+        #frame for checkbox, image
         self.pic_frame = tk.Frame(self)#, background="light green")
         self.pic_frame.grid(row=0, column=0, sticky="nsew")
         #frame for name, weight, button
         self.info_frame = tk.Frame(self)#, background="pink")
         self.info_frame.grid(row=0, column=1, sticky="nsew")
 
-        self.status = tk.StringVar()
-        self.status.set(1)
+        self.status = 0
+        #self.status.set(0)
         self.status_control = tk.Radiobutton(self.pic_frame, variable = self.status, value=0, command=self.activeItem)
         self.status_control.grid(row=0, column=0, padx=15)
 
         self.canvas_width = 100
         self.canvas_height= 100
-        self.pic_canvas = tk.Canvas(self.pic_frame, width=self.canvas_width, height=self.canvas_height)
+        self.pic_canvas = tk.Canvas(self.pic_frame, width=self.canvas_width, height=self.canvas_height, background="white")
         self.pic_canvas.grid(row=0, column=1, padx=15, pady=10)
 
         self.image = self.process_image(item['imgPath'], 102)
@@ -34,11 +34,12 @@ class ItemComponent(tk.Frame):
         #self.testlabel.grid(row=1,column=1,padx=15,pady=10)
         #
         self.itemName_label = tk.StringVar()
-        self.name_label = tk.Label(self.info_frame, textvariable=self.itemName_label, width=15, font=("Arial",12))
-        self.name_label.grid(row=0, padx=15, pady=8, sticky="nsew")
+        self.itemName_label.set(item['name'])
+        self.name_label = tk.Label(self.info_frame, textvariable=self.itemName_label, width=10, fg="#008080",anchor="sw", font=("Arial",15))
+        self.name_label.grid(row=0, padx=20, pady=8, sticky="w")
         #
         self.progressBar = tk.Canvas(self.info_frame, width=150, height=20, background="light grey")
-        self.progressBar.grid(row=1, padx=15, pady=8, sticky="nsew")
+        self.progressBar.grid(row=1, padx=15, pady=4, sticky="nsew")
 
         self.current_weight = 100
         self.progressBar.create_rectangle(0,0,self.current_weight,25, fill="#00C5CD")
@@ -50,7 +51,11 @@ class ItemComponent(tk.Frame):
         pass
 
     def activeItem(self):
+        #self.status.set(self.status_control['value'])
+        #print(self.status_control['value'])
+        #print(self.status)
         pass
+
 
     def process_image(self, path, target_size):
         image = self.load_image(path)
--- PBL/View/MainUI.py
+++ PBL/View/MainUI.py
@@ -7,15 +7,19 @@ from PBL import application
 class MainUI(tk.Frame):
 
     def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent,background="orange")
         self.grid()
         #label = tk.Label(self, text="Start Page")
         #label.grid(row = 0,column = 2)
         self.csvController = csvController.CsvController()
         itemList = self.loadItemData()
+        statusVariable = tk.IntVar()
         if itemList:
-            itemComponent.ItemComponent(self, itemList[0])
-
+            for index,item in enumerate(itemList):
+                itemFrame = itemComponent.ItemComponent(self, itemList[index])
+                itemFrame.status = statusVariable
+                itemFrame.status_control['value']=index
+            #itemFrame.status.set(0)
 
     def loadItemData(self):
         itemList = self.csvController.getItemData()
