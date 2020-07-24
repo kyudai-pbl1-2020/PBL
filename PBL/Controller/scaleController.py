8632910f5fa9b017687b1eb6d47e1a320cca465f
ONourry <onourry3@gmail.com>
2020-07-24 14:35:51 +0900

    Fixed sensor weight processing

--- PBL/Controller/scaleController.py
+++ PBL/Controller/scaleController.py
@@ -35,48 +35,30 @@ class ScaleController:
         time.sleep(0.1)
         self.ser.write(b'SIR\r')
 
-    def escaping(self,c):
-        if c != '\r' and (ord(c)<0x20 or ord(c)> 0x7e):
-            return ''
-        return c
-
-    def dataprocess(self,esc):
-        weight = None
-        escaped_split = esc.split('\r')
-        for line in escaped_split[:-1]:
-            if line[0:2] != 'ST':
-                continue
-            try:
-                weight = float(line[3:12])
-            except:
-                continue
-            #print('{:.2f}'.format(weight),flush=True)
-
-        return weight
+    def process(self,data):
+        data = data.decode()
+        data = data.split(',')[1]
+        data = data.replace("+", "")
+        data = data.strip("0")
+        return data
 
 
     def getWeight(self):
         self.setup()
-
         scale = b''
-        escaped = ''
 
         if(self.ser.in_waiting != 0):
             data = self.ser.read(self.ser.in_waiting)
             scale += data
-        if len(scale) > 0:
-            escaped += ''.join(list(map(self.escaping,scale.decode())))
-            escaped = self.dataprocess(escaped)
-            #scale = b''
 
         self.ser.write(b'C\r')
-        return escaped
+        weight = self.process(scale)
+        return weight
 
 
 #TESTING
 if __name__ == "__main__":
     currentWeight = None
     sc = ScaleController()
-    while currentWeight == None:
-        currentWeight = sc.getWeight()
+    currentWeight = sc.getWeight()
     print(currentWeight)
