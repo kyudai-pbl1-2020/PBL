6b7982528ef3c13ca120360aa4e88338116a1761
郭月晴 <guoyueqing@Mooni.local>
2020-07-24 17:14:30 +0900

    pull from cucumber

--- PBL/View/MainUI.py
+++ PBL/View/MainUI.py
@@ -1,6 +1,4 @@
 import tkinter as tk
-import time
-from PBL.View import addItemView as ai
 from PBL.Controller import csvController
 from .Components import itemComponent
 import os
@@ -29,38 +27,10 @@ class MainUI(tk.Frame):
         itemList = self.loadItemData()
         statusVariable = tk.IntVar()
         if itemList:
-
             for index,item in enumerate(itemList):
                 itemFrame = itemComponent.ItemComponent(self.frame, itemList[index],index)
                 itemFrame.status = statusVariable
                 itemFrame.status_control['value']=index
-
-
-            itemComponent.ItemComponent(self, itemList[0])
-
-        container = tk.Frame(self)
-        canvas = tk.Canvas(container, width=550, height=500)
-        canvas.place(x=100, y=0)
-        scrollbar = tk.Scrollbar(container, orient="vertical", command=canvas.yview)
-        scrollable_frame = tk.Frame(canvas)
-
-        scrollable_frame.bind(
-            "<Configure>",
-            lambda e: canvas.configure(
-                scrollregion=canvas.bbox("all")
-            )
-        )
-
-        canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
-
-        canvas.configure(yscrollcommand=scrollbar.set)
-
-        for i in range(50):
-            tk.Label(scrollable_frame, text="Sample scrolling label").pack()
-
-        container.pack(fill="both", expand=True)
-        canvas.pack(side="left", fill="both", expand=True)
-        scrollbar.pack(side="right", fill="y")
 
 
     def loadItemData(self):
